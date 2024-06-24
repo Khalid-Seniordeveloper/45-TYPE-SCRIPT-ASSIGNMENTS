0f42097178ed14ad63988a12f159a045929cd0e1
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-06-25 01:03:41 +0500

    3RD DONE

---
+++ ASS NO 4/main.js
@@ -0,0 +1,3 @@
+var personName = "ALBERT EINSTEIN";
+var quote = "A person who never made a mistake never tried anything new";
+console.log("ONCE ".concat(personName, " said \"").concat(quote, "\""));
---
+++ ASS NO 4/main.ts
@@ -0,0 +1,5 @@
+let personName = "ALBERT EINSTEIN";
+let quote = "A person who never made a mistake never tried anything new";
+
+
+console.log(`ONCE ${personName} said "${quote}"`);
