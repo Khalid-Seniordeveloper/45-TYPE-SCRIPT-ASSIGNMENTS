efab979d0feb2989593f6d69af15f166348591a4
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-06-25 13:54:55 +0500

    12 DONE

---
+++ ASS NO 12/main.js
@@ -0,0 +1,4 @@
+var nameS = ["KHALID", 'talha', "RAYYAN", 'ERYY'];
+nameS.forEach(function (names) {
+    console.log("".concat(names, " ! have a great day"));
+});
---
+++ ASS NO 12/main.ts
@@ -0,0 +1,6 @@
+let nameS = ["KHALID", 'talha' ,"RAYYAN",'ERYY']
+
+nameS.forEach(names => {
+    console.log(`${names} ! have a great day`);
+    
+})
