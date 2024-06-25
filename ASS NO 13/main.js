c71fa32a5aeace5b1e80c57d29208a9f4006b27c
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-06-25 14:04:33 +0500

    13TH DONE

---
+++ ASS NO 13/main.js
@@ -0,0 +1,4 @@
+var cars = ['HONDA', "BMW", "MERCIDES", "CIVIC"];
+console.log(cars.forEach(function (names) {
+    console.log("I would like to own a ".concat(names, " motorcycle"));
+}));
---
+++ ASS NO 13/main.ts
@@ -0,0 +1,7 @@
+let cars = ['HONDA',"BMW","MERCIDES","CIVIC"];
+
+console.log(cars.forEach(names =>{
+    console.log(`I would like to own a ${names} motorcycle`);
+    
+}
+));
