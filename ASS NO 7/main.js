f59e2d3005d5add08afc88629ed6d5a8b0f05898
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-06-25 12:59:20 +0500

    6TH DINE

---
+++ ASS NO 7/main.js
@@ -0,0 +1,4 @@
+var num1 = 4;
+var num2 = 10;
+var num3 = 2;
+console.log(num1 * num1 - num2 + num1 - num3);
---
+++ ASS NO 7/main.ts
@@ -0,0 +1,6 @@
+let num1 = 4;
+let num2 = 10;
+let num3 = 2;
+
+
+console.log(num1 * num1 - num2 + num1 - num3);
