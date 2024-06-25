5d3f6d8e3daba329bd387e3df5898b30a2c82d3b
ALYARA-MOBILE-ACCESSORIES <alyaramobileaccessories@gmail.com>
2024-06-25 13:39:28 +0500

    11TH DONE

---
+++ ASS NO 11/main.js
@@ -0,0 +1,5 @@
+var names = ["TALHA", "KHALID", "RAYYAN", "WANIYAH"];
+console.log("FRIENDS NAME :");
+names.forEach(function (name) {
+    console.log(name);
+});
---
+++ ASS NO 11/main.ts
@@ -0,0 +1,6 @@
+let names = ["TALHA", "KHALID", "RAYYAN", "WANIYAH"];
+console.log(`FRIENDS NAME :`);
+names.forEach(name => {
+    console.log(name);
+    
+})
